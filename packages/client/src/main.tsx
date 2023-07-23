6d9eae6cf91dbbc93ce97e2e147b0006f127a9e4
Veys Aliyev <veysaliyev00@gmail.com>
2023-07-23 04:07:02 -0400

    chore(client): update project

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import * as ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import App from "./app/app";
+import { router } from "./router";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
