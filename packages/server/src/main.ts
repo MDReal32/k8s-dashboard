801589966cb4ea9181fdca94c23c44ace8ee4520
Veys Aliyev <veysaliyev00@gmail.com>
2023-07-25 02:26:32 -0400

    chore(server): setup ws

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,20 +1,19 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { WsAdapter } from "@nestjs/platform-ws";
 
 import { AppModule } from "./app.module";
+import { RequestInterceptor } from "./interceptors/request.interceptor";
 
-async function bootstrap() {
+const logger = new Logger("ApplicationBootstrap");
+const port = process.env.PORT || 3000;
+const globalPrefix = "api";
+
+(async () => {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
-}
+  app.setGlobalPrefix(globalPrefix).useGlobalInterceptors(new RequestInterceptor());
+  const wsApp = app.useWebSocketAdapter(new WsAdapter(app));
+  await wsApp.listen(port);
 
-bootstrap();
+  logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+})();
