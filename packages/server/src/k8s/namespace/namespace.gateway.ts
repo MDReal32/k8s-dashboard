e519c52139517574005d0c565c1071585e884923
Veys Aliyev <veysaliyev00@gmail.com>
2023-07-25 02:27:51 -0400

    chore(server): suspense warnings

--- packages/server/src/k8s/namespace/namespace.gateway.ts
+++ packages/server/src/k8s/namespace/namespace.gateway.ts
@@ -1,9 +1,4 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { WebSocketGateway } from "@nestjs/websockets";
 
 @WebSocketGateway()
-export class NamespaceGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
-}
+export class NamespaceGateway {}
