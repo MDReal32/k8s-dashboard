2df6a61977e058674aac5194a6e3436cc0d43742
Veys Aliyev <veysaliyev00@gmail.com>
2023-07-27 01:02:56 -0400

    chore(server): git and project modules

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
-import { K8sModule } from './k8s/k8s.module';
+
+import { K8sModule } from "./k8s/k8s.module";
+import { GitModule } from "./git/git.module";
+import { ProjectModule } from "./project/project.module";
 
 @Module({
-  imports: [K8sModule]
+  imports: [K8sModule, GitModule, ProjectModule]
 })
 export class AppModule {}
