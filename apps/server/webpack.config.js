8adc545c85e7db51cf653bfaf234ceff7f201196
Veys Aliyev <veysaliyev00@gmail.com>
2023-07-30 00:34:22 -0400

    chore(server): refactor webpack config

--- apps/server/webpack.config.js
+++ apps/server/webpack.config.js
@@ -1,8 +1,5 @@
-const { composePlugins, withNx} = require('@nx/webpack');
+const { composePlugins, withNx } = require("@nx/webpack");
 
-// Nx plugins for webpack.
-module.exports = composePlugins(withNx(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
+module.exports = composePlugins(withNx(), config => {
   return config;
 });
