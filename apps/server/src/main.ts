c96608d344c72954bdf8f8f69501e20ae944d9c5
Veys Aliyev <veysaliyev00@gmail.com>
2023-07-30 00:30:50 -0400

    Revert "chore(server.main): server secret support. For future validation."
    This reverts commit 8aed285b

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,6 @@
-import { mkdir, writeFile, unlink } from "node:fs/promises";
-import * as crypto from "node:crypto";
-
-import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { WsAdapter } from "@nestjs/platform-ws";
+import { ValidationPipe } from "@nestjs/common";
 
 import { loggerFactory } from "@ugrab/k8s-shared";
 
@@ -15,15 +12,7 @@ const logger = new Logger("ApplicationBootstrap");
 const port = +process.env.SERVER_PORT || 3000;
 const globalPrefix = "api";
 
-const runFile = "/home/mdreal/.cache/ugrab";
-const secretFile = `${runFile}/secret.txt`;
-
-export const sessionSecret = crypto.randomBytes(32).toString("hex");
-
 (async () => {
-  await mkdir(runFile, { recursive: true });
-  await writeFile(secretFile, sessionSecret);
-
   const app = await NestFactory.create(AppModule);
   app
     .setGlobalPrefix(globalPrefix)
@@ -33,9 +22,4 @@ export const sessionSecret = crypto.randomBytes(32).toString("hex");
   await wsApp.listen(port);
 
   logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
-
-  process.on("SIGINT", async () => {
-    await app.close();
-    await unlink(secretFile);
-  });
 })();
