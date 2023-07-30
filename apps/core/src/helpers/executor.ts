23e9090af6b647d66440662e03e190fe41e418e0
Veys Aliyev <veysaliyev00@gmail.com>
2023-07-30 09:19:01 -0400

    chore(core): command executor

---
+++ apps/core/src/helpers/executor.ts
@@ -0,0 +1,51 @@
+import * as child_process from "child_process";
+
+import { Logger } from "../main";
+
+type Extension = (err: Error | string | null, line: string | null) => void;
+
+export class Executor {
+  private readonly _extensions: Set<Extension> = new Set();
+  private readonly logger = new Logger("CommandExecutor");
+
+  extend(fn: Extension) {
+    this._extensions.add(fn);
+    return this;
+  }
+
+  run(command: string | string[]) {
+    const cmd = this.command(command);
+    console.log(cmd);
+    // const child = child_process.exec(cmd);
+    //
+    // this.logger.log(`Executing: ${cmd}`);
+    //
+    // child.stdout
+    //   .on("data", (line: string) => {
+    //     this._extensions.forEach(fn => fn(null, line));
+    //   })
+    //   .on("error", (line: string) => {
+    //     this._extensions.forEach(fn => fn(line, null));
+    //   });
+    //
+    // child.stderr
+    //   .on("data", (line: string) => {
+    //     this._extensions.forEach(fn => fn(line, null));
+    //   })
+    //   .on("error", (line: string) => {
+    //     this._extensions.forEach(fn => fn(line, null));
+    //   });
+    //
+    // child.on("close", (code: number) => {
+    //   this.logger.log(`Command ${cmd} finished with code ${code}`);
+    // });
+    //
+    // return child;
+  }
+
+  private command(command: string | string[]) {
+    return Array.isArray(command) ? command.join(" ") : command;
+  }
+}
+
+export const executor = new Executor();
