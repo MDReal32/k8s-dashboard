565693968e4afa66934da88f8dc91f97f70c3742
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-01 04:48:16 -0400

    chore(shared): update schema

--- libs/shared/src/lib/validation/schemas/project-init.schema.ts
+++ libs/shared/src/lib/validation/schemas/project-init.schema.ts
@@ -1,14 +1,21 @@
 import { z } from "zod";
 
 export const projectInitSchema = z.object({
-  name: z.string(),
-  repo: z.object({
-    url: z.string().url("Invalid URL"),
-    branch: z.string(),
-    ssh: z.boolean()
-  }),
-  ci: z.object({
-    dir: z.string(),
-    provider: z.enum(["docker", "docker-compose", "k8s", "kubernetes", "helm"])
-  })
+  name: z.string({ description: "Name of project" }).optional(),
+  repo: z
+    .object({
+      url: z.string().describe("Cloneable repository url").url("Invalid Url"),
+      branch: z.string().describe("Repository cloning branch").optional(),
+      ssh: z.boolean().describe("Clone via ssh or https").optional()
+    })
+    .optional(),
+  ci: z
+    .object({
+      dir: z.string().describe("CI directory relative to root").optional(),
+      provider: z
+        .enum(["docker", "docker-compose", "k8s", "kubernetes", "helm"])
+        .describe("Pre-defined CI provider")
+        .optional()
+    })
+    .optional()
 });
