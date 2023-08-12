3352409fb5f5189dd075baa1cf6fa975ab644f29
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:31:29 -0400

    chore(core): simple echo logs for all

--- apps/server/src/log/log.service.ts
+++ apps/server/src/log/log.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@nestjs/common";
 
+import { WS_EVENTS } from "@k8sd/shared";
+
 import { LogDto } from "./dto/log.dto";
 import { LogHeaderDto } from "./dto/log-header.dto";
 import { BaseService } from "../base/base.service";
@@ -7,6 +9,7 @@ import { BaseService } from "../base/base.service";
 @Injectable()
 export class LogService extends BaseService {
   receiveLog(data: LogDto, headers: LogHeaderDto) {
-    return { data, headers };
+    // ToDO: Implement by project.id
+    this.broadcastAll(WS_EVENTS.LOG.LOG_LISTEN, data, headers);
   }
 }
