643d9692041b8c8fac65b8221a12dab396ae3c15
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:30:56 -0400

    chore(core): core introduces himself

--- apps/server/src/log/log.gateway.ts
+++ apps/server/src/log/log.gateway.ts
@@ -1,7 +1,12 @@
-import { WebSocketGateway } from "@nestjs/websockets";
+import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+
+import { WS_EVENTS } from "@k8sd/shared";
 
 import { LogService } from "./log.service";
 import { BaseGateway } from "../base/base.gateway";
+import { MessageHeaders } from "./log.decorator";
+import { LogDto } from "./dto/log.dto";
+import { LogHeaderDto } from "./dto/log-header.dto";
 
 @WebSocketGateway({ path: "/ws" })
 export class LogGateway extends BaseGateway {
@@ -9,11 +14,8 @@ export class LogGateway extends BaseGateway {
     super(logService);
   }
 
-  // @SubscribeMessage(WS_EVENTS.LOG.LOG)
-  // @UsePipes(ZodValidationWebsocketPipeFactory(LogDto.schema))
-  // receiveLog(
-  //   @ConnectedSocket() client: WebSocket,
-  //   @MessageBody() data: LogDto,
-  //   @MessageHeaders() headers: LogHeaderDto
-  // ) {}
+  @SubscribeMessage(WS_EVENTS.LOG.LOG)
+  receiveLog(@MessageBody() data: LogDto, @MessageHeaders() headers: LogHeaderDto) {
+    this.logService.receiveLog(data, headers);
+  }
 }
