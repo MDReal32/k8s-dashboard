24250caac501ecb69599e56686c1d698acb741f4
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:34:06 -0400

    chore(server): use prisma module for implementing prisma

--- apps/server/src/project/project.module.ts
+++ apps/server/src/project/project.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "nestjs-prisma";
+
 import { ProjectController } from "./project.controller";
 import { ProjectService } from "./project.service";
 import { ProjectGateway } from "./project.gateway";
-import { GitModule } from "../git/git.module";
 
 @Module({
-  imports: [GitModule],
+  imports: [PrismaModule],
   controllers: [ProjectController],
   providers: [ProjectService, ProjectGateway]
 })
