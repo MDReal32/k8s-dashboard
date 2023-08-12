cac2543363cb126c6bf5636c495ecdfea13139b0
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 13:26:59 -0400

    chore(shared): refactor

--- libs/shared/src/lib/shared.ts
+++ libs/shared/src/lib/shared.ts
@@ -1,7 +1,9 @@
-export * from "./regex";
-
+export * from "./types";
+export * from "./utils";
 export * from "./validation";
 export * from "./ws";
+
+export * from "./regex";
 export * from "./logger";
 export * from "./const";
 export * from "./env";
