a720657413ceb7bfaff1e807a9a0e85c51b56de9
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:37:42 -0400

    chore(server): refactor

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "nestjs-prisma";
 
 import { K8sModule } from "./k8s/k8s.module";
-import { GitModule } from "./git/git.module";
 import { ProjectModule } from "./project/project.module";
-import { LogModule } from './log/log.module';
+import { LogModule } from "./log/log.module";
 
 @Module({
-  imports: [K8sModule, GitModule, ProjectModule, LogModule]
+  imports: [K8sModule, ProjectModule, LogModule, PrismaModule.forRoot()]
 })
 export class AppModule {}
