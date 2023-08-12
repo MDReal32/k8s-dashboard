9c63433231f85df33b2e7ebabc3f256ad30e7f9f
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:25:51 -0400

    chore(core): repo's storage

---
+++ apps/core/src/core/storage.ts
@@ -0,0 +1,35 @@
+import { Stream } from "node:stream";
+import { createWriteStream, mkdirSync, writeFileSync } from "node:fs";
+import { dirname, resolve } from "node:path";
+
+import { STORAGE_ROOT } from "../const";
+
+export class Storage {
+  constructor(private readonly root: string) {}
+
+  getPath(name: string) {
+    return resolve(this.root, name);
+  }
+
+  createFile(path: string, content: string | Buffer | Stream) {
+    const filePath = this.getPath(path);
+    this.createDirectory(dirname(filePath));
+
+    if (content instanceof Stream) {
+      const stream = createWriteStream(filePath);
+      content.pipe(stream);
+      return new Promise<void>((resolve, reject) => {
+        stream.on("finish", resolve);
+        stream.on("error", reject);
+      });
+    }
+
+    return writeFileSync(resolve(this.root, path), content);
+  }
+
+  createDirectory(path: string) {
+    return mkdirSync(this.getPath(path), { recursive: true });
+  }
+}
+
+export const storage = new Storage(STORAGE_ROOT);
