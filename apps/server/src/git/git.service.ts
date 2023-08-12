4e74ea3d0ec5e607929d0de3a5ab5626c9a89e27
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:06:29 -0400

    chore(server): moved to core

--- apps/server/src/git/git.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-import { Logger } from "@ugrab/k8s-shared";
-
-@Injectable()
-export class GitService {
-  private readonly logger = new Logger("GitService");
-}
