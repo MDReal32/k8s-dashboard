adcb8925df9b590064547da53a858760c7414eda
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:22:44 -0400

    chore(core): api for requests

---
+++ apps/core/src/api/api.ts
@@ -0,0 +1,36 @@
+import { fromFetch } from "rxjs/fetch";
+import { Project } from "@prisma/client";
+import { PartialDeep } from "type-fest";
+
+import { projectInitSchema } from "@k8sd/shared";
+
+import { Data } from "../types/data";
+
+export const port = +process.env.SERVER_PORT || 3000;
+export const host = process.env.SERVER_HOST || "localhost";
+export const baseUrl = process.env.BASE_URL || `http://${host}:${port}/api`;
+const url = new URL(baseUrl);
+const wsPathname = process.env.WS_PATH || "/ws";
+const hostname = url.host === "localhost" ? `:${url.port}` : "";
+export const wsUrl = `ws://${url.host}${hostname}${wsPathname}`;
+
+interface Response<TData> {
+  success: boolean;
+  data: TData;
+  error?: null;
+}
+
+export const api = {
+  project: {
+    patch$: (id: string, data: PartialDeep<Data<typeof projectInitSchema, Project>>) =>
+      fromFetch<Response<Data<typeof projectInitSchema, Project>>>(`${baseUrl}/v1/project/${id}`, {
+        selector: response => response.json(),
+        method: "PATCH",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      })
+  }
+};
