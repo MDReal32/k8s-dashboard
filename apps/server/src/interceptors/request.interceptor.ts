e7ba3081a681407b2ec8e2f4270f8ab307a89ed8
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:06:40 -0400

    chore(server): handle exception

--- apps/server/src/interceptors/request.interceptor.ts
+++ apps/server/src/interceptors/request.interceptor.ts
@@ -13,14 +13,19 @@ export class RequestInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
     return next.handle().pipe(
       map(data => ({ success: true, data, error: null })),
-      catchError(async error => ({
-        success: false,
-        data: null,
-        error:
-          error instanceof HttpException
-            ? error.getResponse()
-            : { ...pick(error, ["message"]), status: 500 }
-      }))
+      catchError(error => {
+        throw new HttpException(
+          {
+            success: false,
+            data: null,
+            error:
+              error instanceof HttpException
+                ? error.getResponse()
+                : { ...pick(error, ["message"]), status: 500 }
+          },
+          error.status || 500
+        );
+      })
     );
   }
 }
