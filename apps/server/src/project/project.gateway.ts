47572a8858d7f1f8ebe1ade4ff7224f62d4a50b1
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 05:33:49 -0400

    chore(core): extend over base gateway

--- apps/server/src/project/project.gateway.ts
+++ apps/server/src/project/project.gateway.ts
@@ -1,4 +1,11 @@
 import { WebSocketGateway } from "@nestjs/websockets";
 
+import { BaseGateway } from "../base/base.gateway";
+import { ProjectService } from "./project.service";
+
 @WebSocketGateway({ path: "/ws" })
-export class ProjectGateway {}
+export class ProjectGateway extends BaseGateway {
+  constructor(private readonly projectService: ProjectService) {
+    super(projectService);
+  }
+}
