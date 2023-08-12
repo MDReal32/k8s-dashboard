1f2c54d9d34df772ef7c7334f794f6e2497e9951
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-12 13:32:54 -0400

    chore(plugin-builder): plugin type

--- libs/plugin-builder/src/types/plugin.ts
+++ libs/plugin-builder/src/types/plugin.ts
@@ -1,10 +1,16 @@
 import { Promisable } from "type-fest";
 
-import { Logger } from "@k8sd/shared";
+import { Logger, File, Executor } from "@k8sd/shared";
 
 export interface PluginContext {
+  name: string;
   ciRoot: string;
+
   logger: Logger;
+  executor: Executor;
+
+  find(path: string, file: (file: File) => boolean): File | undefined;
+  task(cb: () => Promise<void>): void;
 }
 
 export interface Plugin {
@@ -13,7 +19,7 @@ export interface Plugin {
   version: string;
   priority?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
-  detect(context: PluginContext): Promisable<boolean>;
+  detect(context: PluginContext): boolean;
 
-  executor(context: PluginContext): Promisable<void>;
+  install(context: PluginContext): Promisable<void>;
 }
