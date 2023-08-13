1bd1c8afd4ad258ed255c93ac6fe701770656af6
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-13 04:57:12 -0400

    chore(core): fix error

--- apps/core/src/main.ts
+++ apps/core/src/main.ts
@@ -1,9 +1,9 @@
 import { Project } from "@prisma/client";
 
 import { projectInitSchema, WS_EVENTS } from "@k8sd/shared";
+import { wsUrl } from "@k8sd/plugin-sdk";
 
 import { WebsocketClient } from "./utils/websocket-client";
-import { wsUrl } from "./api/api";
 import { Core } from "./core";
 import { Steps } from "./core/steps";
 
