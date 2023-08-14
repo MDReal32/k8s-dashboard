e3e5441f04d9e9cd6768000d6cf5475084809659
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-14 03:34:19 -0400

    chore(server): k8s-service doesn't injectable anymore

--- apps/server/src/k8s/k8s.module.ts
+++ apps/server/src/k8s/k8s.module.ts
@@ -1,11 +1,8 @@
 import { Module } from "@nestjs/common";
 
-import { K8sService } from "./k8s.service";
 import { NamespaceModule } from "./namespace/namespace.module";
 
 @Module({
-  imports: [NamespaceModule],
-  providers: [K8sService],
-  exports: [K8sService]
+  imports: [NamespaceModule]
 })
 export class K8sModule {}
