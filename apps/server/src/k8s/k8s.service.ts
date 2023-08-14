625e254a63b6054c17aa189d8134f058b38a07e4
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-14 03:33:12 -0400

    chore(server): extends from basek8s class. Tries to connect default k8s cluster

--- apps/server/src/k8s/k8s.service.ts
+++ apps/server/src/k8s/k8s.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from "@nestjs/common";
 import { CoreV1Api, KubeConfig } from "@kubernetes/client-node";
+import { BadRequestException } from "@nestjs/common";
 
-@Injectable()
-export class K8sService {
+import { Logger } from "@k8sd/shared";
+
+import { BaseK8s } from "../utils/base-k8s";
+
+export class K8sService extends BaseK8s {
   private readonly _kc: KubeConfig;
-  private readonly _k8sApi: CoreV1Api;
+  private _k8sApi: CoreV1Api;
 
-  constructor() {
+  private __retry = 0;
+  private __maxRetry = 1e2;
+
+  constructor(protected readonly logger: Logger) {
+    super();
+
     this._kc = new KubeConfig();
-    this._kc.loadFromDefault();
-    this._k8sApi = this._kc.makeApiClient(CoreV1Api);
+    this.makeApiClient();
   }
 
   get k8sApi(): CoreV1Api {
@@ -18,5 +25,26 @@ export class K8sService {
 
   get kc(): KubeConfig {
     return this._kc;
+  }
+
+  private makeApiClient() {
+    try {
+      this._kc.loadFromDefault();
+      this._k8sApi = this._kc.makeApiClient(CoreV1Api);
+    } catch (error) {
+      if (this.__retry === 0) this.logger.log("Kubernetes API isn't available. Retrying...");
+      else this.logger.log(`Retrying ${this.__retry}/${this.__maxRetry}...`);
+
+      this.__retry++;
+
+      let startTime = performance.now();
+      while (performance.now() - startTime < 1000) {}
+      if (this.__retry > this.__maxRetry) {
+        this.logger.error(error);
+        throw new BadRequestException("Kubernetes API isn't available", { cause: error });
+      }
+
+      return this.makeApiClient();
+    }
   }
 }
