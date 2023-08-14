df37b5ff58812b1cac5d5f0ccde3faa601578e0a
Veys Aliyev <veysaliyev00@gmail.com>
2023-08-14 03:34:38 -0400

    chore(server): extend from k8s-service

--- apps/server/src/k8s/namespace/namespace.service.ts
+++ apps/server/src/k8s/namespace/namespace.service.ts
@@ -1,45 +1,35 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
+
+import { Logger } from "@k8sd/shared";
 
 import { K8sService } from "../k8s.service";
-import { BaseK8s, PodResource, Resource, ServiceResource } from "../../utils/base-k8s";
 import { CreateNamespaceDto } from "./dto/create-namespace.dto";
+import { PodResource, Resource, ServiceResource } from "../../utils/base-k8s";
 
 @Injectable()
-export class NamespaceService extends BaseK8s {
-  constructor(private readonly k8sService: K8sService) {
-    super();
+export class NamespaceService extends K8sService {
+  constructor() {
+    super(new Logger("NamespaceService"));
   }
 
   async getAllNamespaces() {
-    const namespaces = await this.k8sService.k8sApi.listNamespace();
+    const namespaces = await this.catch(this.k8sApi.listNamespace());
     return namespaces.body.items.map(ns => this.getNamespaceResource(ns));
   }
 
   async getNamespace(namespace: string) {
-    try {
-      const ns = await this.k8sService.k8sApi.readNamespace(namespace);
-      return this.getNamespaceResource(ns.body);
-    } catch (e) {
-      throw new BadRequestException(`Namespace ${namespace} not found`, { cause: e });
-    }
+    const ns = await this.catch(this.k8sApi.readNamespace(namespace));
+    return this.getNamespaceResource(ns.body);
   }
 
   async createNamespace(data: CreateNamespaceDto) {
-    try {
-      const ns = await this.k8sService.k8sApi.createNamespace({ metadata: { name: data.name } });
-      return this.getNamespaceResource(ns.body);
-    } catch (e) {
-      throw new BadRequestException(`Namespace ${data.name} already exists`, { cause: e });
-    }
+    const ns = await this.catch(this.k8sApi.createNamespace({ metadata: { name: data.name } }));
+    return this.getNamespaceResource(ns.body);
   }
 
   async deleteNamespace(namespace: string) {
-    try {
-      await this.k8sService.k8sApi.deleteNamespace(namespace);
-      return { message: `Namespace ${namespace} deleted` };
-    } catch (e) {
-      throw new BadRequestException(`Namespace ${namespace} not found`, { cause: e });
-    }
+    await this.catch(this.k8sApi.deleteNamespace(namespace));
+    return { message: `Namespace ${namespace} deleted` };
   }
 
   async getNamespaceResources(namespace: string) {
@@ -56,7 +46,7 @@ export class NamespaceService extends BaseK8s {
       return this.allNamespace(this.getNamespacePods);
     }
 
-    const pods = await this.k8sService.k8sApi.listNamespacedPod(namespace);
+    const pods = await this.catch(this.k8sApi.listNamespacedPod(namespace));
     return pods.body.items.map(pod => this.podResource(pod));
   }
 
@@ -65,7 +55,7 @@ export class NamespaceService extends BaseK8s {
       return this.allNamespace(this.getNamespaceServices);
     }
 
-    const services = await this.k8sService.k8sApi.listNamespacedService(namespace);
+    const services = await this.catch(this.k8sApi.listNamespacedService(namespace));
     return services.body.items.map(service => this.serviceResource(service));
   }
 
