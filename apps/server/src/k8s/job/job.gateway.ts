ff6c69dcc48dfc65e8d2ac8a58b5d46ba2ef7d9f
MDReal32 <veysaliyev00@gmail.com>
2023-10-14 08:53:23 -0400

    chore(server): implement job

--- apps/server/src/k8s/job/job.gateway.ts
+++ apps/server/src/k8s/job/job.gateway.ts
@@ -1,4 +1,11 @@
 import { WebSocketGateway } from "@nestjs/websockets";
 
-@WebSocketGateway()
-export class JobGateway {}
+import { BaseK8sGateway } from "../../base/base-k8s.gateway";
+import { JobService } from "./job.service";
+
+@WebSocketGateway({ path: "/ws/k8s/v1/resource/job" })
+export class JobGateway extends BaseK8sGateway {
+  constructor(private readonly jobService: JobService) {
+    super(jobService);
+  }
+}
