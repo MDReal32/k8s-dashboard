288cc75e62edab6746d325a02fb1a3a4fd2d947b
MDReal32 <veysaliyev00@gmail.com>
2023-10-16 00:20:56 -0400

    chore(client): setup tailwindcss

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@total-typescript/ts-reset";
 
+import "./core.css";
 import { router } from "./routes";
 
 const queryClient = new QueryClient();
