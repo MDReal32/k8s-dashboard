a0becd10381e042ba7b9289f206ee1303b61b0bc
MDReal32 <veysaliyev00@gmail.com>
2023-10-20 04:52:09 -0400

    chore(shared): unnecessary schemas

--- libs/shared/src/lib/validation/schemas/index.ts
+++ libs/shared/src/lib/validation/schemas/index.ts
@@ -1,4 +1,2 @@
 export * from "./log";
 export * from "./project";
-
-export * from "./k8s";
--- libs/shared/src/lib/validation/schemas/k8s/namespace/create-namespace.schema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const createNamespaceSchema = z.object({
-  name: z.string({ description: "Name of namespace" })
-});
