0888b59dc66a6b4249eb2ee7b5dabdcc292306da
MDReal32 <veysaliyev00@gmail.com>
2023-10-20 06:40:51 -0400

    chore(client): fix classes

--- apps/client/src/components/controls/controls.tsx
+++ apps/client/src/components/controls/controls.tsx
@@ -1,5 +1,5 @@
 import { FC, PropsWithChildren } from "react";
 
 export const Controls: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="absolute right-0 top-0 w-40 h-52 bg-gray-500 z-10">{children}</div>;
+  return <div className="absolute right-2 top-2 w-40 h-52 bg-gray-500 z-10">{children}</div>;
 };
