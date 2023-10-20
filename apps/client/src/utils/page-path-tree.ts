c6e2d11d8ef3edbaca3ef64b7370221b2db595b7
MDReal32 <veysaliyev00@gmail.com>
2023-10-20 06:34:54 -0400

    chore(client): update

--- apps/client/src/utils/page-path-tree.ts
+++ apps/client/src/utils/page-path-tree.ts
@@ -4,25 +4,14 @@ import { RouteObject } from "react-router-dom";
 class PagePathBinaryTreeNode {
   constructor(readonly path: string) {}
 
-  declare element: JSX.Element;
-  nodes = new Set<PagePathBinaryTreeNode>();
-
-  findNextNode(
-    param: (node: PagePathBinaryTreeNode) => boolean
-  ): PagePathBinaryTreeNode | undefined {
-    for (const nextNode of this.nodes) {
-      if (param(nextNode)) {
-        return nextNode;
-      }
-    }
-  }
+  declare name: string;
+  declare element: RouteObject["element"];
 
   get routeObject(): RouteObject {
-    return {
-      path: this.path,
-      element: this.element,
-      children: []
-    };
+    const route: RouteObject = { element: this.element };
+    if (this.path) route.path = this.path;
+    if (this.name) route.id = this.name;
+    return route;
   }
 }
 
@@ -47,67 +36,97 @@ type FunctionOrString<T> = T extends `${infer _}:${infer _}`
   : string;
 
 type AsPathMap<T> = {
-  [K in keyof T as MakeAsKey<K & string>]: K extends keyof T ? FunctionOrString<T[K]> : never;
+  [K in keyof T as MakeAsKey<K & string>]: FunctionOrString<T[K]>;
+};
+
+type AsRouteMap<T> = {
+  [K in keyof T as MakeAsKey<K & string>]: K;
 };
 
 export class PagePathTree<T> {
-  private readonly _root = new PagePathBinaryTreeNode("/");
+  private readonly _root = new PagePathBinaryTreeNode("");
   private readonly _pathNodeMap = new Map<string, PagePathBinaryTreeNode>();
+  private readonly _names = new Set<string>();
 
   addRoute<K extends string>(
+    name: string,
     route: K,
     element: JSX.Element
   ): PagePathTree<T & { [TK in K as TK]: TK }>;
-  addRoute<K extends keyof T>(route: K, element: JSX.Element): this;
+  addRoute<K extends keyof T>(name: string, route: K, element: JSX.Element): this;
   addRoute<K extends string>(
+    name: string,
     route: K,
     element: JSX.Element
   ): PagePathTree<T & { [TK in K as TK]: TK }> | this {
     const path = route as unknown as string;
+    if (this._names.has(name)) throw new Error(`The name "${name}" is already in use.`);
 
-    const pathParts = path.split("/").filter(part => part !== "");
-    let currentNode = this._root;
-    let wholePath = "";
-
-    for (const pathPart of pathParts) {
-      wholePath += `/${pathPart}`;
-      const nextNode = currentNode.findNextNode(node => node.path === wholePath);
-
-      if (nextNode) {
-        currentNode = nextNode;
-        continue;
-      }
-
-      const newNode = new PagePathBinaryTreeNode(wholePath);
-      currentNode.nodes.add(newNode);
-      currentNode = newNode;
-    }
-
-    currentNode.element = element;
-    this._pathNodeMap.set(path, currentNode);
+    let node = this._pathNodeMap.get(path) || new PagePathBinaryTreeNode(path);
+    node.name = name;
+    node.element = element;
+    this._pathNodeMap.set(path, node);
+    this._names.add(name);
 
     return this;
   }
 
+  setLayout(layout: RouteObject["element"]) {
+    this._root.element = layout;
+    return this;
+  }
+
+  getRoutePath(path: string) {
+    const node = this._pathNodeMap.get(path);
+    if (!node) throw new Error(`The path "${path}" does not exist.`);
+    return node.routeObject;
+  }
+
   getRoutes() {
-    return this._getRoutes(this._root);
+    const routes = Array.from(this._pathNodeMap.values()).map(node => node.routeObject);
+
+    if (this._root.element) {
+      const rootElement = this._root.routeObject;
+      delete rootElement.path;
+      rootElement.children = routes;
+      return [rootElement];
+    }
+
+    return routes;
+  }
+
+  getRoutePaths() {
+    const routeMap = {} as AsRouteMap<T>;
+
+    for (const [path] of this._pathNodeMap) {
+      const urlVariables = path.match(/\/:[^/]+/g);
+      let key = path.replace(/\//g, "_").replace(/^_/, "");
+
+      if (urlVariables) {
+        const vars = new Set<string>();
+        urlVariables.forEach(variable => {
+          vars.add(variable.replace(/^\/:/, ""));
+          key = key.replace(variable.slice(1), variable.replace(/^\/:/, ""));
+        });
+      }
+
+      routeMap[(key || "__root__").toUpperCase() as keyof typeof routeMap] = path as any;
+    }
+
+    return Object.freeze(routeMap);
   }
 
   getPathMap() {
     const pathMap = {} as AsPathMap<T>;
 
-    for (const [path, node] of this._pathNodeMap) {
-      if (node.nodes.size > 0 && !node.element) {
-        continue;
-      }
-
+    for (const [path] of this._pathNodeMap) {
       const urlVariables = path.match(/\/:[^/]+/g);
       let key = path.replace(/\//g, "_").replace(/^_/, "");
       let pathFn = path as (typeof pathMap)[keyof typeof pathMap];
 
       if (urlVariables) {
         const vars = new Set<string>();
-        urlVariables.map(variable => {
+        urlVariables.forEach(variable => {
           vars.add(variable.replace(/^\/:/, ""));
           key = key.replace(variable.slice(1), variable.replace(/^\/:/, ""));
         });
@@ -125,29 +144,9 @@ export class PagePathTree<T> {
         }) as any;
       }
 
-      pathMap[key.toUpperCase() as keyof typeof pathMap] = pathFn;
+      pathMap[(key || "__root__").toUpperCase() as keyof typeof pathMap] = pathFn;
     }
 
     return Object.freeze(pathMap);
-  }
-
-  private _getRoutes(node: PagePathBinaryTreeNode): RouteObject[] {
-    const routes: RouteObject[] = [];
-
-    if (node.element) {
-      const route = node.routeObject;
-
-      for (const nextNode of node.nodes) {
-        route.children?.push(...this._getRoutes(nextNode));
-      }
-
-      routes.push(route);
-    } else {
-      for (const nextNode of node.nodes) {
-        routes.push(...this._getRoutes(nextNode));
-      }
-    }
-
-    return routes;
   }
 }
