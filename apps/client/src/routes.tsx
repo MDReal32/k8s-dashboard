778ef692a8308097c8e9090d2382eba79474bfa7
MDReal32 <veysaliyev00@gmail.com>
2023-10-20 06:41:20 -0400

    chore(client): pages

--- apps/client/src/routes.tsx
+++ apps/client/src/routes.tsx
@@ -2,10 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { ApplicationLayout } from "./layout/application-layout";
 import { Graph } from "./pages/graph";
+import { Main } from "./pages/main";
+import { Project } from "./pages/project";
 import { PagePathTree } from "./utils/page-path-tree";
 
 export const routeBuilder = new PagePathTree()
   .setLayout(<ApplicationLayout />)
+  .addRoute("Main", "/", <Main />)
+  .addRoute("Projects", "/projects", <Project />)
   .addRoute("Graph", "/graph", <Graph />)
   .addRoute("Graph (:namespace)", "/graph/:namespace", <Graph />);
 
