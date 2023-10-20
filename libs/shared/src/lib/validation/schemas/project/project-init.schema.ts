1054ea9a0e21701f8a9cd620d7fec533f9fe4525
MDReal32 <veysaliyev00@gmail.com>
2023-10-20 04:53:59 -0400

    chore(shared): Update schema

--- libs/shared/src/lib/validation/schemas/project/project-init.schema.ts
+++ libs/shared/src/lib/validation/schemas/project/project-init.schema.ts
@@ -1,35 +1,14 @@
 import { z } from "zod";
-import { $Enums } from "@prisma/client";
 
-import { GITHUB_VALIDATE_RE } from "../../../regex";
+import { GITHUB_CREDENTIALS_RE, GITHUB_VALIDATE_RE } from "../../../regex";
 
 export const projectInitSchema = z.object({
   name: z.string({ description: "Name of project" }),
-  status: z
-    .enum([
-      $Enums.Status.PROJECT_CREATING,
-      $Enums.Status.PROJECT_INITIALIZING,
-      $Enums.Status.PROJECT_INITIALIZED,
-      $Enums.Status.RETRIEVING_PROVIDER,
-      $Enums.Status.INSTALLING_PROVIDER,
-      $Enums.Status.PROVIDER_INSTALLED
-    ])
-    .optional(),
-  repo: z
-    .object({
-      url: z
-        .string()
-        .describe("Cloneable repository url")
-        .url("Invalid Url")
-        .regex(GITHUB_VALIDATE_RE, "Invalid Github repository url"),
-      branch: z.string().describe("Repository cloning branch").optional(),
-      ssh: z.boolean().describe("Clone via ssh or https").optional()
-    })
-    .optional(),
-  ci: z
-    .object({
-      dir: z.string().describe("CI directory relative to root").optional(),
-      provider: z.string().describe("Pre-defined CI provider").optional()
-    })
-    .optional()
+  repo: z.object({
+    url: z.string().regex(GITHUB_VALIDATE_RE, "Must be a valid GitHub URL"),
+    credentials: z
+      .object({ username: z.string(), password: z.string() })
+      .or(z.string().regex(GITHUB_CREDENTIALS_RE))
+  }),
+  ci: z.object({ type: z.string() })
 });
