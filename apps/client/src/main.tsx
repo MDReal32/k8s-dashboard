858f3b580025e94513c2c9d3ac369e256d9fcc7b
MDReal32 <veysaliyev00@gmail.com>
2023-10-20 06:29:19 -0400

    chore(client): remove tanstack react query

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,18 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@total-typescript/ts-reset";
 
 import "./core.css";
 import { router } from "./routes";
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
