e13dcfe827db42418ef4bd2adb5730288392a541
MDReal32 <veysaliyev00@gmail.com>
2023-10-20 04:55:12 -0400

    chore(shared): add credential regex

--- libs/shared/src/lib/regex.ts
+++ libs/shared/src/lib/regex.ts
@@ -1,2 +1,4 @@
 export const GITHUB_VALIDATE_RE =
   /^https?:\/\/github\.com\/(?<owner>[a-zA-Z0-9_-]+)\/(?<repo>[a-zA-Z0-9_-]+)$/;
+
+export const GITHUB_CREDENTIALS_RE = /^(?<username>\w+)@(?<password>\w+)$/;
