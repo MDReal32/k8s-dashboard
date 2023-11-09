ab5fdffdef7454b48ee94dbc52422451506fc219
MDReal32 <veysaliyev00@gmail.com>
2023-11-09 08:32:14 -0500

    chore(client): update ui on useTheme hook

--- apps/client/src/hooks/use-theme.ts
+++ apps/client/src/hooks/use-theme.ts
@@ -2,22 +2,23 @@ import { useCallback, useEffect, useState } from "react";
 import { useLocalStorage } from "react-use";
 
 type Theme = "light" | "dark";
+const htmlContainer = document.querySelector("html")!;
 const initialTheme: Theme = window.matchMedia("(prefers-color-scheme: dark)").matches
   ? "dark"
   : "light";
 
 export const useTheme = () => {
-  const [theme = "light", setTheme] = useLocalStorage<Theme>("theme", initialTheme);
-  const [dataTheme, setDataTheme] = useState<Theme>(theme);
+  const [theme, setTheme] = useLocalStorage<Theme>("theme", initialTheme, { raw: true });
+  const [dataTheme, setDataTheme] = useState<Theme>(() => theme || "light");
 
   useEffect(() => {
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => {
-      setTheme(e.matches ? "dark" : "light");
+      setDataTheme(e.matches ? "dark" : "light");
     });
   }, []);
 
   useEffect(() => {
-    document.body.dataset.theme = dataTheme;
+    htmlContainer.dataset.theme = dataTheme;
     setTheme(dataTheme);
   }, [dataTheme]);
 
