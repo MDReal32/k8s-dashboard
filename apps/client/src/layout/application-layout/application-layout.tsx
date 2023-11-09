5f6566035d04fefbeb35c9e1af57889e4aab43a9
MDReal32 <veysaliyev00@gmail.com>
2023-11-09 08:33:54 -0500

    chore(client): extended application layout added navbar

--- apps/client/src/layout/application-layout/application-layout.tsx
+++ apps/client/src/layout/application-layout/application-layout.tsx
@@ -1,6 +1,73 @@
 import { FC } from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+
+import { Container } from "../../components/container";
+import { Navbar } from "../../components/navbar";
+import { NavbarLink } from "../../components/navbar-link";
+import { SwitchTheme } from "../../components/switch-theme";
+import { routePaths } from "../../routes";
 
 export const ApplicationLayout: FC = () => {
-  return <Outlet />;
+  return (
+    <>
+      <Container transition>
+        <header>
+          <Navbar>
+            <Navbar.Brand as={Link} to="/">
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="mr-3 h-8"
+                alt="Flowbite Logo"
+              />
+              <span className="self-center whitespace-nowrap text-2xl font-semibold">
+                K8S Dashboard
+              </span>
+            </Navbar.Brand>
+
+            <Navbar.GetStarted className="flex gap-2">
+              <Navbar.Toggle>
+                <svg
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </Navbar.Toggle>
+
+              <SwitchTheme>
+                <SwitchTheme.Light>Light Theme</SwitchTheme.Light>
+                Dark Theme
+              </SwitchTheme>
+            </Navbar.GetStarted>
+
+            <Navbar.Collapse width={230} toggleSide="right">
+              <NavbarLink paths={[routePaths.__ROOT__]} />
+              <NavbarLink paths={[routePaths.PROJECTS]} />
+              <NavbarLink
+                paths={[routePaths.GRAPH, routePaths.GRAPH_NAMESPACE]}
+                updateVariable={namespace => (namespace === "_" ? "All" : namespace)}
+              />
+            </Navbar.Collapse>
+          </Navbar>
+        </header>
+      </Container>
+
+      <div className="relative h-[2000px] w-full">
+        <Outlet />
+      </div>
+
+      <Container transition>
+        <footer></footer>
+      </Container>
+    </>
+  );
 };
